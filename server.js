725eeb6ee06a716a24a80be4d3e8eae9e84a98dc
Harish <harishkr0055@gmail.com>
2024-11-11 12:14:58 +0530

    stop the infiite reloading of page by sending a response

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const server = http.createServer( (req, res)=> {
     // checking the request method
     // req.url will give the route of the site
     console.log(req.url);
+
+
+    // Now the website will not reload infinitely because I have sent a response.
+    res.end("Noice");
 })
 
 // listening server on a port
